513521908452af1db2e6760dcf09de4846b18dd3
vidhi03work-png <vidhi03.work@gmail.com>
2026-06-13 23:20:37 +0530

    Add files via upload

---
+++ SESSION 2.sql
@@ -0,0 +1,9 @@
+USE SQL_ASS;
+
+SELECT*FROM restaurants;
+
+select review_id,rating from zomato_reviews;
+
+select movie_name as title, release_year as Year_Released from movies;
+
+select *from products;
